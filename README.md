a2ea9d00a093414c7833f342bba983e49e1f1cad
John Nathaniel Jr <johnzy33@gmail.com>
2023-10-17 18:48:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# FCEI
+
+This repository serves as a the default space for the building of the proposed college education system implement in flutter and Rust using Domian Driven Design architecture. libraries via `flutter_rust_bridge`.
+
+## Getting Started
+
+## License
+
+Copyright 2023 C.Vegas.
+
+This template is licensed under either of
+
+- [Apache License, Version 2.0](https://www.apache.org/licenses/LICENSE-2.0) ([LICENSE-APACHE](LICENSE-APACHE))
+- [MIT license](https://opensource.org/licenses/MIT) ([LICENSE-MIT](LICENSE-MIT))
+
+at your option.
+
+The [SPDX](https://spdx.dev/) license identifier for this project is `MIT OR Apache-2.0`.
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'ffi.dart' if (dart.library.html) 'ffi_web.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'education system',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'education system'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+extension<L, R> on (FutureOr<L>, FutureOr<R>) {
+  // A convenience method enabled by Dart 3, which will be useful later.
+  Future<(L, R)> join() async {
+    final fut =
+        await Future.wait([Future.value(this.$1), Future.value(this.$2)]);
+    return (fut[0] as L, fut[1] as R);
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<Platform> platform;
+  late Future<bool> isRelease;
+
+  @override
+  void initState() {
+    super.initState();
+    platform = api.platform();
+    isRelease = api.rustReleaseMode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("You're running on"),
+            FutureBuilder(
+              future: (platform, isRelease).join(),
+              builder: (context, snap) {
+                final style = Theme.of(context).textTheme.headlineMedium;
+                if (snap.error != null) {
+                  debugPrint(snap.error.toString());
+                  return Tooltip(
+                    message: snap.error.toString(),
+                    child: Text('Unknown OS', style: style),
+                  );
+                }
+
+                // Guard return here, the data is not ready yet.
+                final data = snap.data;
+                if (data == null) return const CircularProgressIndicator();
+
+                final (platform, release) = data;
+                final releaseText = release ? 'Release' : 'Debug';
+
+                final text = switch (platform) {
+                  Platform.Android => 'Android',
+                  Platform.Ios => 'iOS',
+                  Platform.MacApple => 'MacOS with Apple Silicon',
+                  Platform.MacIntel => 'MacOS',
+                  Platform.Windows => 'Windows',
+                  Platform.Unix => 'Unix',
+                  Platform.Wasm => 'the Web',
+                  Platform.Unknown => 'Unknown OS',
+                };
+                return Text('$text ($releaseText)', style: style);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
