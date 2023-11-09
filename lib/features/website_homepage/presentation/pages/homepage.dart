6e041dfa9fc78141c7e41af438c04439cee1f9f0
John Nathaniel Jr <johnzy33@gmail.com>
2023-11-09 18:31:22 +0100

    init homepage views creation & fix for issue #001

---
+++ lib/features/website_homepage/presentation/pages/homepage.dart
@@ -0,0 +1,5 @@
+//TODO: Integrate the themes file for this page.
+
+//* Highligts: Please the pages should contain purly only view components, every widget components should be moved to the widget view part
+
+//! Alerts: All functions and class should be wrritten to handle one and one purpuse alone, this is to help in writting test
---
+++ lib/features/website_homepage/presentation/widgets/homepagewidget.dart
@@ -0,0 +1,1 @@
+//! Note all widget component for homepage page view is to be written here. 
