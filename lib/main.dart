b6410571ab91a301cbf3c550b5bb13724f12c257
UtibeEtim <etim.utibe@gmail.com>
2024-02-12 11:39:14 +0100

    Refactor SurrealDB in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,15 @@
-import 'package:education_system/core/enum/user_role.dart';
-import 'package:education_system/core/model/student_model.dart';
+import 'package:education_system/src/rust/api/features/website_homepage/data/surreal_db/surreal_db_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:surrealdb/surrealdb.dart';
-import 'package:uuid/uuid.dart';
-import 'config/network/network_config.dart';
 import 'config/routes/locator.dart';
 import 'config/routes/navigation.dart';
 import 'config/routes/routes.dart';
-import 'package:education_system/src/rust/frb_generated.dart';
 import 'src/rust/api/features/website_homepage/presentation/pages/home/home_page.dart';
 
 void main() async {
   dependenciesInjectorSetup();
-  var uuid = Uuid();
-  const options = SurrealDBOptions();
-  final client = SurrealDB('ws://localhost:8000/rpc', options: options)
-    ..connect();
-
-  await client.wait();
-  await client.use('test', 'test');
-  await client.signin(user: 'root', pass: 'root');
-
-  final student = await client.create(
-      'student',
-      StudentModel(
-          id: uuid.v8(),
-          name: Name(firstName: "Daniel", lastName: "Frank"),
-          gender: "Male",
-          dateOfBirth: "20|03|2000",
-          bloodGroup: "O+",
-          email: "email@gmail.com",
-          phoneNumber: "+23480457684",
-          jambRegNumber: "",
-          contactNo: "21 Ademsy address",
-          role: "UserRole.student.toString()",
-          emergencyContactNo: "23480457600",
-          presentAddress: "21 Present address",
-          permanentAddress: "21 Permanent address",
-          guardian: GuardianModel(
-              fatherName: "Frank",
-              fatherOccupation: "Teacher",
-              fatherContactNo: "+23470345678",
-              motherName: "Grace",
-              motherOccupation: "Mini Seller",
-              motherContactNo: "23470345622",
-              address: "Mothers Address"),
-          localGuardian: LocalGuardianModel(
-              name: "Frank",
-              occupation: "Local man",
-              contactNo: "23470345678",
-              address: "Local man address"),
-          profileImage: "",
-          academicFaculty: "Science",
-          academicDepartment: "Microbiology",
-          academicSemester: "First Semester",
-          createdAt: "DateTime.now()",
-          updatedAt: "DateTime.now()"));
-
-  // final person2 = await client.create('student', {
-  //   'name': "Morgan Hitchcock",
-  //   'occupation': "Local person2",
-  //   'contactNo': "23470345678",
-  //   'address': "Local person2 address"
-  // });
-
-  // final persons = await client.select<Map<String, dynamic>>('student');
-
-  // final groupByQuery = await client.query(
-  //   r'SELECT name, count() FROM type::table($tb) GROUP BY name',
-  //   {
-  //     'tb': 'student',
-  //   },
-  // );
-
-  debugPrint("Student: $student ||\n");
-  // debugPrint("Student2: $person2 ||\n");
-  // debugPrint("Student3: $person3 ||\n");
-  // debugPrint("Students: $persons ||\n");
-  // debugPrint("GroupByQuery: $groupByQuery ||\n");
-  //client.close();
+  SurrealDBClient().init();
+  await SurrealDBClient().signIn();
 
   runApp(const MyApp());
 }
--- lib/src/rust/api/features/website_homepage/data/surreal_db/surreal_db_client.dart
+++ lib/src/rust/api/features/website_homepage/data/surreal_db/surreal_db_client.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:surrealdb/surrealdb.dart';
+import 'package:bcrypt/bcrypt.dart';
+import '../../../../../../../core/model/student_model.dart';
 
 class SurrealDBClient {
   static final SurrealDBClient _instance = SurrealDBClient._internal();
@@ -18,5 +21,53 @@ class SurrealDBClient {
     await client.use('test', 'test');
     await client.signin(user: 'root', pass: 'root');
   }
+
+  Future<bool> loginWithEmail(String email, String password) async {
+    final result = await SurrealDBClient().client.select(
+        'student',
+    //     where: {
+    //   'email': email,
+    //   // Assume you store hashed passwords and compare hashes
+    //   'password': hashPassword(password),
+    // }
+    );
+
+    if (result.isNotEmpty) {
+      // Login successful
+      return true;
+    } else {
+      // Login failed
+      return false;
+    }
+  }
+
+  String hashPassword(String password) {
+    // Hash a password using bcrypt with a cost of 10
+    final hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+    return hashedPassword;
+  }
+
+  bool verifyPassword(String password, String hashedPassword) {
+    // Verify the password against the hashed version
+    return BCrypt.checkpw(password, hashedPassword);
+  }
+
+
+
+  Future<void> signUp() async {
+    await client.wait();
+    await client.use('test', 'test');
+    await client.signup(
+        user: 'root',
+        pass: 'root',
+
+    );
+  }
+
+  Future<void> registerStudent(/*StudentModel student*/) async {
+    final createdStudent = await SurrealDBClient().client.create('student', student);
+    debugPrint("Student created: $createdStudent");
+  }
+
 
 }
